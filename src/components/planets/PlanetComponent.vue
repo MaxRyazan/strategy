bb2c60d16b3083e251b721b4f6c0b469227cac77
MaxRyazan <no4ta87@mail.ru>
2023-10-08 00:09:21 +0300

    1.4.61  - добавил склад на планету.  - добавил отображение склада на планете  - добавил переключение между складом и строительством

--- src/components/planets/PlanetComponent.vue
+++ src/components/planets/PlanetComponent.vue
@@ -75,14 +75,20 @@
                     <div class="storage__materials">
                         <reusable-text class="storage__column--title">Материалы</reusable-text>
                         <div style="display: flex;width: 100%; justify-content: space-between">
-                            <reusable-text style="width: 60%;  background-color: lightblue; color:black;border-top: 1px solid black;padding: 1px">Название</reusable-text>
-                            <reusable-text style="width: 20%;  background-color: lightblue; color:black;border-top: 1px solid black;padding: 1px">Количество</reusable-text>
-                            <reusable-text style="width: 20%;  background-color: lightblue; color:black;border-top: 1px solid black;padding: 1px">Вес</reusable-text>
+                            <reusable-text class="item__name" style="cursor:default;  border: 1px dotted rgba(0,0,0, .3) ;  background-color: lightblue; color:black;border-top: 1px solid black;padding: 1px">Название</reusable-text>
+                            <reusable-text class="item__count" style="cursor:default;  border: 1px dotted rgba(0,0,0, .3) ; background-color: lightblue; color:black;border-top: 1px solid black;padding: 1px">Количество</reusable-text>
+                            <reusable-text class="item__weight" style="cursor:default;  border: 1px dotted rgba(0,0,0, .3) ;background-color: lightblue; color:black;border-top: 1px solid black;padding: 1px">Вес</reusable-text>
                         </div>
-                        <div style="display: flex; padding: 3px 20px; justify-content: space-between" v-for="material in storageMaterials" :key="material.id">
-                            <div style="width: 60%;">{{material.name}}</div>
-                            <div style="width: 20%;">{{material.count}}</div>
-                            <div style="width: 20%;">{{material.weight * material.count}}</div>
+                        <div style="display: flex; height: 100%;"  v-for="material in storageMaterials" :key="material.id">
+                            <div class="storage_item item__name">
+                                <reusable-text style="width: 100%; display: block; color: white; padding: 3px 20px;">{{ material.name }}</reusable-text>
+                            </div>
+                            <div class="storage_item item__count">
+                                <reusable-text style="width: 100%; display: block; color: white; padding: 3px">{{ material.count}}</reusable-text>
+                            </div>
+                            <div class="storage_item item__weight">
+                                <reusable-text style="width: 100%; display: block; color: white; padding: 3px 20px;">{{ material.count * material.weight}}</reusable-text>
+                            </div>
                         </div>
                     </div>
                     <div class="storage__modules">
@@ -100,7 +106,7 @@ import PlanetDescriptionCard from '@/components/planets/PlanetDescriptionCard.vu
 import ReusableDialog from "@/components/reusable/containers/ReusableDialog.vue";
 import {usePlanetStore} from "@/pinia/planetStore.ts";
 import ReusableButton from "@/components/reusable/buttons/Reusable-button.vue";
-import {BuildingCategory, Materials, StorageEntitiesCategory, TopMenu} from "@/typescript/enums.ts";
+import {BuildingCategory, StorageEntitiesCategory, TopMenu} from "@/typescript/enums.ts";
 import AdministrativeBuildings from "@/components/planets/AdministrativeBuildings.vue";
 import EnergyBuildings from "@/components/planets/EnergyBuildings.vue";
 import ManufacturerBuildings from "@/components/planets/ManufacturerBuildings.vue";
@@ -194,7 +200,20 @@ function showCategory(category: BuildingCategory){
   height: 100%;
   text-align: center;
   overflow: hidden;
-
+}
+.storage_item{
+  height: 100%;
+  width: 100%;
+  border: 1px dotted rgba(173, 216, 230, .3)
+}
+.item__name{
+  width: 60%;
+}
+.item__count{
+  width: 20%;
+}
+.item__weight{
+  width: 20%;
 }
 .storage__column--title{
   display: block;
