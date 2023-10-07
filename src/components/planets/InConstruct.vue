81239f75e2d5e0b4639f69e5302f51d53fbe1331
MaxRyazan <no4ta87@mail.ru>
2023-10-07 17:46:59 +0300

    1.4.6  - переделал интервал строительства зданий

--- src/components/planets/InConstruct.vue
+++ src/components/planets/InConstruct.vue
@@ -1,16 +1,41 @@
 <script setup lang="ts">
 import {BuildingsInConstruct} from "@/typescript/types.ts";
+import {onMounted, ref, watch} from "vue";
 
 const props = defineProps<{
     item: BuildingsInConstruct
 }>()
+const readyIn = ref()
+const prettyReadyTime = ref()
+let interval: any;
+
+onMounted(() => {
+    readyIn.value = new Date(props.item.willReadyAt - Date.now())
+    prettyReadyTime.value = (readyIn.value.getMinutes()<10 ? '0'+readyIn.value.getMinutes():readyIn.value.getMinutes()) + ':' + (readyIn.value.getSeconds()<10 ? '0'+readyIn.value.getSeconds():readyIn.value.getSeconds())
+    interval = setInterval(() => {
+        readyIn.value = new Date(props.item.willReadyAt - Date.now())
+        prettyReadyTime.value = (readyIn.value.getMinutes()<10 ? '0'+readyIn.value.getMinutes():readyIn.value.getMinutes()) + ':' + (readyIn.value.getSeconds()<10 ? '0'+readyIn.value.getSeconds():readyIn.value.getSeconds())
+    }, 1000)
+})
+
+
+
+
+watch(readyIn, (value) => {
+    if(value.getMinutes() <= 0 && value.getSeconds() <= 0) {
+        clearInterval(interval)
+        prettyReadyTime.value = '00:00'
+    }
+})
+
+
 </script>
 
 <template>
     <div style="display: flex; justify-content: space-between; padding: 2px 20px;">
         <div>{{item.id}}</div>
         <div>{{item.building.name}}</div>
-        <div>{{new Date(item.willReadyAt)}}</div>
+        <div>{{ prettyReadyTime }}</div>
     </div>
 </template>
 
