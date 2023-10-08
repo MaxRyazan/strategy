73525360b740909a40c7aa991e818ad2968ae5c1
MaxRyazan <no4ta87@mail.ru>
2023-10-08 19:13:20 +0300

    1.5.2  - изменен интерфейс выбора планеты

--- src/components/header/HeaderControlsButtons.vue
+++ src/components/header/HeaderControlsButtons.vue
@@ -1,7 +1,11 @@
 <template>
-    <reusable-button @push="$emit('openPlanet', player?.account.homePlanet)">{{player?.account.homePlanet.name}}</reusable-button>
-    <div v-for="planet in player?.account.colonies" :key="planet.id">
-        <reusable-button @push="$emit('openPlanet', planet)">{{planet.name}}</reusable-button>
+    <reusable-button @push="isPlanetListOpen=true" style=" width: 75px;" v-if="!isPlanetListOpen">+</reusable-button>
+    <reusable-button @push="isPlanetListOpen=false" style=" width: 75px;" v-else>-</reusable-button>
+    <div class="player_colonies" v-if="isPlanetListOpen">
+        <reusable-button style="width: 100%;" @push="$emit('openPlanet', player?.account.homePlanet)">{{player?.account.homePlanet.name}}</reusable-button>
+        <div v-for="planet in player?.account.colonies" :key="planet.id">
+            <reusable-button style="width: 100%;" @push="$emit('openPlanet', planet)">{{planet.name}}</reusable-button>
+        </div>
     </div>
 </template>
 
@@ -10,9 +14,11 @@ import {usePlayerStore} from "@/pinia/playerStore.ts";
 import {onMounted, Ref, ref} from "vue";
 import {Player} from "@/typescript/classes/Player.ts";
 import {Planet} from "@/typescript/classes/Planet.ts";
+import ReusableButton from "@/components/reusable/buttons/Reusable-button.vue";
 
 const playerStore: any = usePlayerStore()
 const player: Ref<Player> = ref() as Ref<Player>
+const isPlanetListOpen = ref(false)
 onMounted(() => {
     player.value = playerStore.player
 })
@@ -21,5 +27,11 @@ defineEmits<{
 }>()
 </script>
 <style lang="scss" scoped>
-
+.player_colonies{
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 1px;
+  width: 75px;
+}
 </style>
